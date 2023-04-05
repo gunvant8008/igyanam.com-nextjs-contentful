94b471bb8be46a41a23919cca6cefa5a67bf9aad
Gunvant Sharma <43336633+gunvant8008@users.noreply.github.com>
2023-04-05 21:12:46 +0100

    added types in next api folder in req res

--- src/pages/api/test.tsx
+++ src/pages/api/test.tsx
@@ -1,8 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { getHomePageData } from "@/datalayer/contentful/homePage"
+import { NextApiRequest, NextApiResponse } from "next"
 
-export default async function handler(req, res) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const response = await getHomePageData()
   res.status(200).json(response)
 }
